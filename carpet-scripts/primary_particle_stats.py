2857a12a53363cb8a2a1984700e9d0adc7572344
Igor Vaiman <gosha.vaiman@gmail.com>
2021-01-10 18:55:03 +0300

    ascii plotting added to primary particle stats script;

--- carpet-scripts/primary_particle_stats.py
+++ carpet-scripts/primary_particle_stats.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+import numpy as np
+import termplotlib as tpl
 
 from pycoast import CorsikaReader
 
-DAT_FILES_DIR = Path('/storage/vol4/ksenia/CORSIKA_LIBRARY/PHOTONS')
+# DAT_FILES_DIR = Path('/storage/vol4/ksenia/CORSIKA_LIBRARY/PHOTONS')
+DAT_FILES_DIR = Path('/home/njvh')
 
 
 thetas = []
@@ -14,7 +17,7 @@ for dat_file in DAT_FILES_DIR.iterdir():
     if not dat_file.is_file():
         continue
 
-    reader = CorsikaReader(dat_file, verbosity=3)
+    reader = CorsikaReader(str(dat_file), verbosity=3)
 
     for run in reader.runs():
         for shower in run.showers():
@@ -25,6 +28,24 @@ for dat_file in DAT_FILES_DIR.iterdir():
 
 print('Z First:\n')
 print('\n'.join(str(i) for i in zfs))
+
+counts, bin_edges = np.histogram(zfs)
+fig = tpl.figure()
+fig.hist(counts, bin_edges, force_ascii=False)
+fig.show()
+
 print('\n==========================\n\n')
 print('Coords:\n')
 print('\n'.join(f"{th}, {ph}" for th, ph in zip(thetas, phis)))
+
+counts, bin_edges = np.histogram(thetas)
+fig = tpl.figure()
+fig.hist(counts, bin_edges, force_ascii=False)
+fig.show()
+print(f"thetas between {np.min(thetas)} and {np.max(thetas)}")
+
+counts, bin_edges = np.histogram(phis)
+fig = tpl.figure()
+fig.hist(counts, bin_edges, force_ascii=False)
+fig.show()
+print(f"phis between {np.min(phis)} and {np.max(phis)}")

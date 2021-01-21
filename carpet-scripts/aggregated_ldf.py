010ba944170331b11fa439a24d48b17b624ee43f
Igor Vaiman <gosha.vaiman@gmail.com>
2021-01-22 01:22:51 +0300

    showers metadata saving and debug run with all plots at once

--- carpet-scripts/aggregated_ldf.py
+++ carpet-scripts/aggregated_ldf.py
@@ -22,33 +22,39 @@ E_MINUS_ID = 3
 
 
 bin_count = 60
-radii_bin_counts = np.zeros((bin_count,))
-radii_bin_counts_squared = np.zeros_like(radii_bin_counts)
-radii_bin_edges = np.logspace(0, 3, bin_count + 1)  # m
+rad_bin_counts = np.zeros((bin_count,))
+rad_bin_counts_squared = np.zeros_like(rad_bin_counts)
+rad_bin_edges = np.logspace(0, 3, bin_count + 1)  # m
 
-ring_areas = pi * (np.power(radii_bin_edges[1:], 2) - np.power(radii_bin_edges[:-1], 2))  # m ^ 2
+ring_areas = pi * (np.power(rad_bin_edges[1:], 2) - np.power(rad_bin_edges[:-1], 2))  # m ^ 2
 
 
 dat_file_paths = [f for f in DAT_FILES_DIR.iterdir() if f.is_file()]
 showers_count = 0
+SHOWER_META_FNAME = TEMP_FILES / 'meta.dat'
+
+fig, ax = plt.subplots()
 
 for dat_file in tqdm(dat_file_paths):
     reader = CorsikaReader(str(dat_file), verbosity=0)
     for run in reader.runs():
         for shower in run.showers():
+            temp_file = temp_filename(dat_file.name, run, shower)
+            with open(SHOWER_META_FNAME, 'a') as metadat_file:
+                metadat_file.write(f"{temp_file}, {shower.theta}, {shower.z_first}")
+
             if shower.theta > THETA_MAX:
                 continue
             showers_count += 1
 
-            temp_file = temp_filename(dat_file.name, run, shower)
             if (TEMP_FILES / temp_file).exists():
                 # reading cached file
                 with open(TEMP_FILES / temp_file) as f:
                     f.readline()
-                    new_counts = []
+                    new_rad_bin_counts = []
                     for line in f:
-                        new_counts.append(int(line.split()[2]))
-                    new_counts = np.array(new_counts)
+                        new_rad_bin_counts.append(int(line.split()[2]))
+                    new_rad_bin_counts = np.array(new_rad_bin_counts)
             else:
                 shower_x = []
                 shower_y = []
@@ -58,36 +64,37 @@ for dat_file in tqdm(dat_file_paths):
                         shower_y.append(particle.y / 100)  # cm -> m
 
                 particle_radii = np.sqrt(np.power(np.array(shower_x), 2) + np.power(np.array(shower_y), 2))
-                new_counts, _ = np.histogram(particle_radii, radii_bin_edges, density=False)
+                new_rad_bin_counts, _ = np.histogram(particle_radii, rad_bin_edges, density=False)
                 with open(TEMP_FILES / temp_file, 'w') as f:
                     f.write('rad_min\trad_max\tcount\n')
-                    for rad_min, rad_max, count in zip(radii_bin_edges[:-1], radii_bin_edges[1:], new_counts):
+                    for rad_min, rad_max, count in zip(rad_bin_edges[:-1], rad_bin_edges[1:], new_rad_bin_counts):
                         f.write(f"{rad_min}\t{rad_max}\t{count}\n")
 
-            radii_bin_counts += new_counts
-            radii_bin_counts_squared += np.power(new_counts, 2)
+            ax.plot(rad_bin_edges[:-1], new_rad_bin_counts)
+            rad_bin_counts += new_rad_bin_counts
+            rad_bin_counts_squared += np.power(new_rad_bin_counts, 2)
 
-radii_bin_counts_mean = radii_bin_counts / showers_count
+rad_bin_counts_mean = rad_bin_counts / showers_count
 
 bias_correction = (showers_count / (showers_count-1))
 # bias_correction = 1  # for local testing
-radii_bin_counts_std = np.sqrt(  # M[x^2] - M[x]^2
-    bias_correction * ((radii_bin_counts_squared / showers_count) - np.power(radii_bin_counts_mean, 2))
+rad_bin_counts_std = np.sqrt(  # M[x^2] - M[x]^2
+    bias_correction * ((rad_bin_counts_squared / showers_count) - np.power(rad_bin_counts_mean, 2))
 )
 
-radii_area_densities_mean = radii_bin_counts_mean / ring_areas
-radii_area_densities_std = radii_bin_counts_std / ring_areas
+rad_area_densities_mean = rad_bin_counts_mean / ring_areas
+rad_area_densities_std = rad_bin_counts_std / ring_areas
 
-radii_bin_centers = 0.5 * (radii_bin_edges[:-1] + radii_bin_edges[1:])
+rad_bin_centers = 0.5 * (rad_bin_edges[:-1] + rad_bin_edges[1:])
 
-fig, ax = plt.subplots()
-ax.plot(radii_bin_centers, radii_area_densities_mean, color='red')
-ax.fill_between(
-    radii_bin_centers,
-    radii_area_densities_mean-radii_area_densities_std,
-    radii_area_densities_mean+radii_area_densities_std,
-    alpha=0.3, color='red'
-)
+# fig, ax = plt.subplots()
+# ax.plot(rad_bin_centers, rad_area_densities_mean, color='red')
+# ax.fill_between(
+#     rad_bin_centers,
+#     rad_area_densities_mean-rad_area_densities_std,
+#     rad_area_densities_mean+rad_area_densities_std,
+#     alpha=0.3, color='red'
+# )
 # ax.y_scale('log')
 ax.set_yscale('log')
 ax.set_xscale('log')

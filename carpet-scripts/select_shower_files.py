f98c68da8de110f9793d7389f8e7c26786d6d8f7
Igor Vaiman <gosha.vaiman@gmail.com>
2021-02-13 14:16:47 +0300

    Debugging via stdin added, dedicated file streams created for boyth outputs

--- carpet-scripts/select_shower_files.py
+++ carpet-scripts/select_shower_files.py
@@ -7,7 +7,8 @@ from pycoast import CorsikaReader
 
 DAT_FILES_DIR = Path('./data')
 
-OUTPUT_FILE = Path('corsika_files_metadata.tsv')
+FILES_METADATA_FILE = Path('corsika_files_metadata.tsv')
+FILE_NAMES_FILE = Path('corsika_files.txt')
 
 
 E_MIN = 10**5  # GeV
@@ -15,16 +16,19 @@ E_MAX = 10**6  # GeV
 THETA_MAX = pi * 50 / 180  # 50 deg
 
 
-with open(OUTPUT_FILE, 'w') as out:
-    out.write('filename\tID0\tE0\ttheta0\tphi0\n')
+print("processing...")
+
+
+with open(FILES_METADATA_FILE, 'w') as meta, open(FILE_NAMES_FILE, 'w') as names:
+    meta.write('filename\tID0\tE0\ttheta0\tphi0\n')
     for dat_file in DAT_FILES_DIR.iterdir():
         if not dat_file.is_file():
             continue
 
-        reader = CorsikaReader(str(dat_file), verbosity=0)
+        reader = CorsikaReader(str(dat_file), verbosity=2)
 
         for run in reader.runs():
             for shower in run.showers():
                 if shower.energy > E_MIN and shower.energy < E_MAX and shower.theta < THETA_MAX:
-                    print(dat_file.name)
-                    out.write(f"{dat_file.name}\t{shower.particle_id}\t{shower.energy}\t{shower.theta}\t{shower.phi}\n")
+                    names.write(f"{dat_file.name}\n")
+                    meta.write(f"{dat_file.name}\t{shower.particle_id}\t{shower.energy}\t{shower.theta}\t{shower.phi}\n")
